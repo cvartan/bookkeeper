3cbe17f822076dae5c27b79c98858e1f0b58fff1
Constantine <cvartan@gmail.com>
2025-04-08 10:55:47 +0300

    Adding default password for user

--- src/main/resources/db/migration/V3__Add_user.sql
+++ src/main/resources/db/migration/V3__Add_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO bk.users ("name",login,pass_hash,is_blocked) VALUES
-	 ('Ordinary user','user','',false);
+	 ('Ordinary user','user','$2a$10$G8a9NEEX.GiK8DCah.PJ5.wm9yJCSvxOf97o6IhkfyuYj0CfUlYkS',false);
 
 INSERT INTO bk.user_privilegies (user_id,privilege_name) VALUES
 	 ((SELECT id FROM bk.users WHERE login='user') ,'USER');
