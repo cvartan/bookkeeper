c8883e425f89cbb948d953b90316f391ce1c7f31
Constantine <cvartan@gmail.com>
2025-04-17 11:36:05 +0300

    Atomic changes

--- src/main/java/pro/cvartan/test/bookkeeper/api/DictionaryController.java
+++ src/main/java/pro/cvartan/test/bookkeeper/api/DictionaryController.java
@@ -21,8 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
-
 @RestController
 @RequiredArgsConstructor
 public class DictionaryController {
