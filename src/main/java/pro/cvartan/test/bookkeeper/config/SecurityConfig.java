8e50b07ec67a26d3132c20cd05cf0a3b8dad8dea
Constantin <cvartan@gmail.com>
2025-12-07 12:38:58 +0300

    Atomic changes

--- src/main/java/pro/cvartan/test/bookkeeper/config/SecurityConfig.java
+++ src/main/java/pro/cvartan/test/bookkeeper/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.ldap.authentication.BindAuthenticator;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
-import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
